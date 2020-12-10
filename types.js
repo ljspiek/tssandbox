aa24ed901dbeb1bea20b3c38f4ba1db85ede5370
Laura Spieker <laura.spieker@nerdery.com>
2020-12-10 16:02:19 -0600

    practices ts classes, functions, interfaces, types

---
+++ types.js
@@ -0,0 +1,17 @@
+// export{}
+var myString;
+var myNum;
+var myBool;
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
+myString = "Hello World";
+myNum = 22;
+myBool = true;
+strArr = ['Hello', 'world'];
+numArr = [1, 2, 3, 4];
+boolArr = [true, false, true];
+strNumTuple = ['Hello', 4];
+var myVoid = null;
+console.log(myVoid);
---
+++ types.ts
@@ -0,0 +1,24 @@
+// export{}
+
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+let strNumTuple: [string, number]
+
+myString = "Hello World";
+myNum = 22;
+myBool = true;
+strArr = ['Hello', 'world'];
+numArr= [1, 2, 3, 4];
+boolArr= [true, false, true]
+
+strNumTuple=['Hello', 4]
+
+let myVoid: void = null;
+
+console.log(myVoid)
